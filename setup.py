1f1ce2f4bb8eb02840f170f83ad8bfd52904ef57
Dmytro Popovych <drudim.ua@gmail.com>
2014-12-03 22:49:57 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+CQLSL
+-----
+
+CQL for Python without any additional abstraction layers.
+Designed to be simple and fast tool for communication with Cassandra.
+CQLSL is something between native Cassandra driver (which is really awesome!) and ORM.
+
+[Installation](https://github.com/drudim/cqlsl#installation)
+
+[Documentation](https://github.com/drudim/cqlsl#getting-started)
+"""
+from setuptools import setup
+import cqlsl
+
+
+setup(
+    name='cqlsl',
+    version=cqlsl.__version__,
+    url='https://github.com/drudim/cqlsl',
+    license='MIT',
+    author='Dmytro Popovych',
+    author_email='drudim.ua@gmail.com',
+    description='CQL for Python without any additional abstraction layers',
+    long_description=__doc__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Database :: Front-Ends",
+        "Operating System :: OS Independent",
+    ],
+    packages=['cqlsl'],
+    include_package_data=True,
+    install_requires=['cassandra-driver >= 2.1.1'],
+)
